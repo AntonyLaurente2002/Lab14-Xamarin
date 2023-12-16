5462ef2f3c41e1d88d9b97dbbccc00e4b99f7dce
AntonyLaurente2002 <95670297+AntonyLaurente2002@users.noreply.github.com>
2023-12-15 22:30:03 -0500

    Add files via upload

---
+++ Database2022/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Tinoco14.DataContext;
+using Tinoco14.Interfaces;
+using Tinoco14.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tinoco14
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            GetContext().Database.EnsureCreated();
+            MainPage = new MatriculaView();
+        }
+        public static AppDbContext GetContext()
+        {
+            string DbPath = DependencyService.Get<IConfigDataBase>().GetFullPath("efCore.db");
+
+            return new AppDbContext(DbPath);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
